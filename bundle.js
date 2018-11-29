aaaf02746c4931a7baae48012dcab7d75d52361e
brunoamil <brunoamil@localhost.localdomain>
2018-11-29 10:07:10 -0200

    finalizando algumas configuracoes do projeto|

--- bundle.js
+++ bundle.js
@@ -1,31 +1,154 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+//usando Object Short Syntax
+var nome = 'Diego';
+var idade = 23;
+var usuario = {
+  // quando voce tem o nome da mesma variavel, nao é necessario colocar nome: nome, voce omite.
+  nome: nome,
+  idade: idade,
+  empresa: 'Rocketseat'
+};
+console.log(usuario); //=============== comentarios de código anterior ==================
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+/*
+// usando template literal
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+const nome = 'Bruno';
+const sobrenome = 'Lima';
 
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome}.`);
 
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
+// USANDO SPREAD
+
+ const  arr1 = [1,2,3,4];
+ const arr2 = [3,4,5,6];
+
+ const arr3 = [ ...arr1, ...arr2];
+
+ console.log(arr3);
+
+ //USANDO SPREAD 2 
+
+ const usuario1 = {
+     nome: 'Diego',
+     idade: 23,
+     empresa: 'Rocketseat',
+ };
+
+ const usuario2 = {...usuario1, nome: 'Gabriel'};
+
+ console.log(usuario2);
+
+// usando o REST
+
+const usuario = {
+    nome: 'Bruno',
+    idade: 23,
+    empresa: 'Rocketseat'
+};
+
+const { nome, ...resto} = usuario;
+
+console.log(nome);
+console.log(resto);
+
+//usando rest 2
+
+const arr = [1,2,3,4,5];
+
+const [a, b, ...c] = arr;
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+//usando rest 3
+
+function soma (a, b, c) {
+    return a + b + c;
+}
+
+console.log(soma(1,3, 4));
+
+//usando rest 4
+
+function soma (...params) {
+    return params.reduce((total, next) => total + next);
+}
+console.log(soma(1,3,4));
+
+
+const usuario = {
+    nome: 'bruno',
+    idade :'22',
+    endereco: {
+        cidade: 'slz',
+        estado: 'MA'
+    },
+};
+
+ // desestruturaçao 
+const { nome, idade, endereco: { cidade } } = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+//desestruturacao 2 
+function mostraNome({ nome, idade }) {
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
+
+
+
+
+
+const arr = [1,2,3,4,5,6];
+
+const newArr = arr.map(item  =>  item * 2);
+
+console.log(newArr);
+
+const teste = () => ({ nome: 'bruno'});
+
+console.log(teste());
+
+const arr = [1,3,4,5,8,9];
+
+const newArr = arr.map(function(item, index){
+    return item + index;
+}); 
+
+console.log(newArr); 
+
+
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 2;
+});
+
+console.log(find);
+class Matematica {
+    static soma(a, b) {
+        return a + b;
     }
-  }]);
+}
 
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2)); //=============== comentarios de código anterior ==================
-
-/*class List {
+console.log(Matematica.soma(1,2));
+class List {
     constructor() {
         this.data = [];
     }
--- main.js
+++ main.js
@@ -1,3 +1,155 @@
+
+
+
+//usando Object Short Syntax
+
+const nome = 'Diego';
+const idade = 23;
+
+const usuario = {
+    
+    // quando voce tem o nome da mesma variavel, nao é necessario colocar nome: nome, voce omite.
+    nome,
+    idade,
+    empresa: 'Rocketseat'
+};
+
+console.log(usuario);
+
+//=============== comentarios de código anterior ==================
+
+
+
+/*
+// usando template literal
+
+const nome = 'Bruno';
+const sobrenome = 'Lima';
+
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome}.`);
+
+// USANDO SPREAD
+
+ const  arr1 = [1,2,3,4];
+ const arr2 = [3,4,5,6];
+
+ const arr3 = [ ...arr1, ...arr2];
+
+ console.log(arr3);
+
+ //USANDO SPREAD 2 
+
+ const usuario1 = {
+     nome: 'Diego',
+     idade: 23,
+     empresa: 'Rocketseat',
+ };
+
+ const usuario2 = {...usuario1, nome: 'Gabriel'};
+
+ console.log(usuario2);
+
+// usando o REST
+
+const usuario = {
+    nome: 'Bruno',
+    idade: 23,
+    empresa: 'Rocketseat'
+};
+
+const { nome, ...resto} = usuario;
+
+console.log(nome);
+console.log(resto);
+
+//usando rest 2
+
+const arr = [1,2,3,4,5];
+
+const [a, b, ...c] = arr;
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+//usando rest 3
+
+function soma (a, b, c) {
+    return a + b + c;
+}
+
+console.log(soma(1,3, 4));
+
+//usando rest 4
+
+function soma (...params) {
+    return params.reduce((total, next) => total + next);
+}
+console.log(soma(1,3,4));
+
+
+const usuario = {
+    nome: 'bruno',
+    idade :'22',
+    endereco: {
+        cidade: 'slz',
+        estado: 'MA'
+    },
+};
+
+ // desestruturaçao 
+const { nome, idade, endereco: { cidade } } = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+//desestruturacao 2 
+function mostraNome({ nome, idade }) {
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
+
+
+
+
+
+const arr = [1,2,3,4,5,6];
+
+const newArr = arr.map(item  =>  item * 2);
+
+console.log(newArr);
+
+const teste = () => ({ nome: 'bruno'});
+
+console.log(teste());
+
+const arr = [1,3,4,5,8,9];
+
+const newArr = arr.map(function(item, index){
+    return item + index;
+}); 
+
+console.log(newArr); 
+
+
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 2;
+});
+
+console.log(find);
 class Matematica {
     static soma(a, b) {
         return a + b;
@@ -5,8 +157,7 @@ class Matematica {
 }
 
 console.log(Matematica.soma(1,2));
-//=============== comentarios de código anterior ==================
-/*class List {
+class List {
     constructor() {
         this.data = [];
     }
